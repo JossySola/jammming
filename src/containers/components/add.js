d339363801aa04dc71cdcb248f43cf932a40d0e2
Jossy Solá <jossysola@gmail.com>
2023-10-27 00:07:56 -0600

    Completes add button presentational component

--- src/containers/components/add.js
+++ src/containers/components/add.js
@@ -1,7 +1,21 @@
 import React from "react";
 
-export default function Add() {
+export default function Add({obj, playlist, setPlaylist}) {
+    function handleClick() {
+        if(playlist.length > 0) {
+            for(let x of playlist) {
+                if(obj.id === x.id) {
+                    return false;
+                }
+            }
+        }
+        setPlaylist([...playlist, obj]);
+    }
+    
     return (
-        <button>+</button>
+        <button onClick={(e) => {
+            e.preventDefault;
+            handleClick();
+        }}>+</button>
     )
 }
