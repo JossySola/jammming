8b1cdc9ad96d6a7ce21b53c70c64755ec8279dfb
Jossy Solá <jossysola@gmail.com>
2023-10-27 00:05:09 -0600

    Completes stateful component

--- src/containers/search.js
+++ src/containers/search.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Song from "./song.js";
+import requestAPI from "./logic/request.js";
 
-export default function Search() {
+export default function Search({playlist, setPlaylist}) {
+    const [search, setSearch] = useState("");
+    let data = requestAPI(search);
+    
+    const mapping = (arr) => {
+        let jsx = [];
+        for(let obj of arr) {
+            jsx.push(<Song btn="add" obj={obj} setPlaylist={setPlaylist} playlist={playlist} key={obj.id}/>);
+        }
+        return jsx;
+    }
+
     return (
         <>
-            <input type="search"></input>
-            <Song />
+            <input type="search" onChange={(e) => {
+                e.preventDefault();
+                setSearch(e.target.value);
+            }} />
+
+            { search ? null : <span>Start searching for your fav songs!</span> }
+            { data ? mapping(data) : null }
         </>
     )
 }
