2d37db9b7534339e2b1a81d908aeb60806036f41
Jossy Solá <jossysola@gmail.com>
2023-10-28 00:41:03 -0600

    Sets challenge logic. Encodes random verifier with SHA256 algorithm. Creates a 64 based ASCII string from SHA256 encode

--- src/containers/logic/challenge.js
+++ src/containers/logic/challenge.js
@@ -5,10 +5,12 @@ const sha256 = async(plain) => {
     const data = encoder.encode(plain);
     // Uint8Array {0: 101, 1: 72, 2: 88, 3: 83, 4: 67,...}
     return window.crypto.subtle.digest('SHA-256', data);
+    // A digest is a short fixed-length value derived from some variable-length input
 }
 // returns a Promise
 
 const base64encode = (input) => {
+    // The btoa() method creates a Base64-encoded ASCII string from a binary string
     return btoa(String.fromCharCode(...new Uint8Array(input)))
     .then(/=/g, '')
     .then(/\+/g, '-')
