ccf0f288907a70eb45e3a2d98eff8e9a8038d797
Jossy Solá <jossysola@gmail.com>
2023-10-29 18:57:09 -0600

    Adds comments from MDN documentation

--- src/containers/logic/token.js
+++ src/containers/logic/token.js
@@ -1,6 +1,13 @@
 import verifier from './verifier.js';
 import codeChallenge from './challenge.js';
 
+/*
+In security protocols, a CODE CHALLENGE is some data sent to the client by the server
+in order to generate a different response each time. Challenge-response protocols are
+one way to fight against replay attackswhere an attacker listens to the previous
+messages and resends them at a later time to get the same credentials as the original
+message.
+*/
 export const clientId = '4d78daad576446d79d1d038ddb3c3d2a';
 // Given in Spotify developer dashboard
 export const redirectUri = 'http://localhost:3000/';
@@ -30,7 +37,11 @@ authUrl.search = new URLSearchParams(params).toString();
 // URL.search is a search string, also called a query string, that is a string containing a '?' followed
 // by the parameters of the URL
 
-window.location.href = authUrl.toString();
+try {
+    window.location.href = authUrl.toString();
+} catch(e) {
+    console.log(e);
+}
 // HREF is a stringifier that returns a string containing the whole URL, and allows the href to be updated
 // Setting the value of href navigates to the provided URL
 
