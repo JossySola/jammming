d3d1ad0fcb6a74c5e0e8f202ca5cb43d0d83290e
Jossy Solá <jossysola@gmail.com>
2023-10-29 19:19:28 -0600

    Adds function to get Access Token

--- src/containers/logic/access.js
+++ src/containers/logic/access.js
@@ -1,5 +1,7 @@
 import { code, clientId, redirectUri } from "./token.js";
 
+const codeParam = code;
+
 const getAccess = async code => {
     let codeVerifier = localStorage.getItem('code_verifier');
 
@@ -11,15 +13,21 @@ const getAccess = async code => {
         body: new URLSearchParams({
             client_id: clientId,
             grant_type: 'authorization_code',
-            code,
+            codeParam,
             redirect_uri: redirectUri,
             code_verifier: codeVerifier,
 
         }),
     }
 
-    const body = await fetch(url, payload);
-    const response = await body.json();
-
-    localStorage.setItem('access_token', response.access_token);
+    try {
+        const body = await fetch(url, payload);
+        const response = await body.json();
+        localStorage.setItem('access_token', response.access_token);
+    } catch(e) {
+        console.error(e.message);
+        console.error(e.value);
+    }
 }
+
+export default getAccess;
