2a574a0795c19a4d44d955738610e3c660163cf0
Jossy Solá <jossysola@gmail.com>
2023-11-01 16:34:29 -0600

    Completes func for user authorization and access token

--- src/containers/logic/auth.js
+++ src/containers/logic/auth.js
@@ -1,24 +1,34 @@
 import codeChallenge from "./challenge.js";
+import codeVerifier from "./verifier.js";
 import auth0 from "./Auth0.js";
 
 const clientId = '4d78daad576446d79d1d038ddb3c3d2a';
-const redirectUri = 'http://localhost:3000';
+const redirectUri = 'http://localhost:3000/';
 
-const scope = 'user-read-private user-read-email';
-const authUrl = new URL("https://accounts.spotify.com/authorize")
+function userAuth() {
+  const scope = 'user-read-private user-read-email';
+  const authUrl = new URL("https://accounts.spotify.com/authorize");
+  const state = auth0;
 
-// generated in the previous step
-window.localStorage.setItem('code_verifier', codeVerifier);
+  // generated in the previous step
+  window.localStorage.setItem('state', state);
+  window.localStorage.setItem('code_verifier', codeVerifier);
 
-const params =  {
-  response_type: 'code',
-  client_id: clientId,
-  redirect_uri: redirectUri,
-  scope,
-  state: auth0,
-  code_challenge_method: 'S256',
-  code_challenge: codeChallenge,
+  const params =  {
+    response_type: 'code',
+    client_id: clientId,
+    redirect_uri: redirectUri,
+    scope,
+    state: state,
+    code_challenge_method: 'S256',
+    code_challenge: codeChallenge,
+  }
+
+  authUrl.search = new URLSearchParams(params).toString();
+  window.location.href = authUrl.toString();
+  const urlParams = new URLSearchParams(window.location.search);
+  let code = urlParams.get('code');
+  window.localStorage.setItem('code', code);
 }
 
-authUrl.search = new URLSearchParams(params).toString();
-window.location.href = authUrl.toString();
+export {userAuth, clientId, redirectUri};
