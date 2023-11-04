9ce2a25d3f8eda95e16d501549c4f99a223310b9
Jossy Solá <jossysola@gmail.com>
2023-11-03 21:26:19 -0600

    Adds functions to handle user decline and user authorization URL parameters

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
 import React, { useState } from "react";
 import Search from "./containers/search.jsx";
 import Playlist from "./containers/playlist.jsx";
-
+import accessToken from "./containers/logic/access.js";
 
 export default function App() {
     const [playlist, setPlaylist] = useState([]);
+    
+    const declined = () => {
+        const urlParams = new URLSearchParams(window.location.search);
+        if(urlParams.get('error')) {
+            return <span>Spotify authorization is required</span>
+        }
+    }
+
+    const redirected = () => {
+        const urlParams = new URLSearchParams(window.location.search);
+        if(urlParams.get('code')) {
+            console.log(localStorage)
+            accessToken();
+        }
+    }
+
     return (
         <>
+            {redirected()}
+            <span>{declined()}</span>
+
             <main>
                 <Search playlist={playlist} setPlaylist={setPlaylist}/>
             </main>
