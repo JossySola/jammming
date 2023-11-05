04a1ef4a02a8377fbeff8f8c8ae4d522da5ab3ed
Jossy Solá <jossysola@gmail.com>
2023-11-05 13:10:57 -0600

    Relocates getting the URL parameter 'code' to the App Hook to be passed as a param into accessToken()

--- src/containers/logic/auth.js
+++ src/containers/logic/auth.js
@@ -1,18 +1,13 @@
-import codeChallenge from "./challenge.js";
-import codeVerifier from "./verifier.js";
-import auth0 from "./Auth0.js";
-
 const clientId = '4d78daad576446d79d1d038ddb3c3d2a';
 const redirectUri = 'http://localhost:3000/';
 
-function userAuth() {
+function userAuth(challenge, state) {
   const scope = 'user-read-private user-read-email';
   const authUrl = new URL("https://accounts.spotify.com/authorize");
-  const state = auth0;
 
-  // generated in the previous step
-  window.localStorage.setItem('state', state);
-  window.localStorage.setItem('code_verifier', codeVerifier);
+  localStorage.setItem('code_challenge', challenge);
+  localStorage.setItem('state', state);
+  console.log(state)
 
   const params =  {
     response_type: 'code',
@@ -21,14 +16,14 @@ function userAuth() {
     scope,
     state: state,
     code_challenge_method: 'S256',
-    code_challenge: codeChallenge,
+    code_challenge: challenge,
   }
 
   authUrl.search = new URLSearchParams(params).toString();
   window.location.href = authUrl.toString();
-  const urlParams = new URLSearchParams(window.location.search);
-  let code = urlParams.get('code');
-  window.localStorage.setItem('code', code);
+  //const urlParams = new URLSearchParams(window.location.search);
+  //let code = urlParams.get('code');
+  //window.localStorage.setItem('code', code);
 }
 
-export {userAuth, clientId, redirectUri};
+export { userAuth, clientId, redirectUri };
