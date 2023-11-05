948743df0935ca98e1a802af77411a8b11ec35b7
Jossy Solá <jossysola@gmail.com>
2023-11-05 13:12:07 -0600

    Comments out the removeItem codes in response errors

--- src/containers/logic/access.js
+++ src/containers/logic/access.js
@@ -1,59 +1,68 @@
 import { clientId, redirectUri } from "./auth.js";
 import getRefreshToken from "./refresh.js";
-import { userAuth } from "./auth.js";
 
-export default function accessToken() {
-    const urlParams = new URLSearchParams(window.location.search);
-    let codeVerifier = localStorage.getItem('code_verifier');
-    const code = urlParams.get('code');
-    const err = urlParams.get('error');
-    const state = urlParams.get('state');
+export default function accessToken(code, state, verifierParam) {
     const localState = localStorage.getItem('state');
 
     if(state === localState) {
-        if(err) {
-            window.localStorage.removeItem('code_verifier');
-            window.localStorage.removeItem('state');
-            window.localStorage.removeItem('code');
-            window.localStorage.removeItem('refresh_token');
-            console.error({
-                type: 'Permissions',
-                message: 'The permissions were not granted.'
-            });
-            return null;
-        } else {
-            const getAccess = async () => {
-                const payload = {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/x-www-form-urlencoded'
-                    },
-                    body: new URLSearchParams({
-                        client_id: clientId,
-                        grant_type: 'authorization_code',
-                        code,
-                        redirect_uri: redirectUri,
-                        code_verifier: codeVerifier
-                    }),
-                }
+        const getAccess = async () => {
+            const payload = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                body: new URLSearchParams({
+                    client_id: clientId,
+                    grant_type: 'authorization_code',
+                    code,
+                    redirect_uri: redirectUri,
+                    code_verifier: verifierParam
+                }),
+            }
+
+            try {
                 const body = await fetch('https://accounts.spotify.com/api/token', payload);
                 const response = await body.json();
                 localStorage.setItem('access_token', response.access_token);
                 localStorage.setItem('refresh_token', response.refresh_token);
+
+                if(response.status !== 200) {
+                    throw new Error(response.message, {
+                        cause: response.status
+                    })
+                }
                 return response;
+
+            } catch(err) {
+                console.log(err.message + ' ' + err.cause);
+                if(err.cause === 401) {
+                    window.localStorage.removeItem('access_token');
+                    window.localStorage.removeItem('code_verifier');
+                    window.localStorage.removeItem('code');
+                    window.localStorage.removeItem('refresh_token');
+                    window.localStorage.removeItem('state');
+                
+                    userAuth();
+                } else if(err.cause === 400) {
+                    /*
+                    window.localStorage.removeItem('access_token');
+                    window.localStorage.removeItem('code_verifier');
+                    window.localStorage.removeItem('code');
+                    window.localStorage.removeItem('refresh_token');
+                    window.localStorage.removeItem('state');*/
+                }
             }
-            getAccess();
         }
+        getAccess();
     } else {
-        window.localStorage.removeItem('code_verifier');
-        window.localStorage.removeItem('state');
-        window.localStorage.removeItem('code');
-        window.localStorage.removeItem('refresh_token');
-        window.localStorage.removeItem('access_token');
-        console.error({
-            type: 'Cross-site Request Forgery prevention.',
-            message: 'The local state is not the same as the one received from server.'
+    /*window.localStorage.removeItem('code_verifier');
+    window.localStorage.removeItem('state');
+    window.localStorage.removeItem('code');
+    window.localStorage.removeItem('refresh_token');
+    window.localStorage.removeItem('access_token');*/
+    console.error({
+        type: 'Cross-site Request Forgery prevention.',
+        message: 'The local state is not the same as the one received from server.'
         });
-        userAuth();
     }
 }
