3dd33af22f8f4ae762cb8915d5efd75fd6e6ab6e
Jossy Solá <jossysola@gmail.com>
2023-11-10 17:42:21 -0600

    Edits return statement with real data from API

--- src/containers/song.jsx
+++ src/containers/song.jsx
@@ -2,15 +2,26 @@ import React from "react";
 import Add from "./components/add.jsx";
 import Remove from "./components/remove.jsx";
 
-export default function Song({btn, obj, setPlaylist, playlist}) {
+export default function Song({btn, setPlaylist, playlist, id, name, preview, album, artists}) {
     return (
         <>
             <details>
-                <span className="songName">{obj.name}</span>
-                <span className="songAuthor">Author: {obj.author}</span>
-                <span className="songGenre">Genre: {obj.genre}</span>
-                {btn === "add" ? <Add obj={obj} playlist={playlist} setPlaylist={setPlaylist}/> : null}
-                {btn === "remove" ? <Remove obj={obj} playlist={playlist} setPlaylist={setPlaylist}/> : null}
+                <summary>{name}</summary>
+                <img 
+                src={album.images[1].url} 
+                width={album.images[1].width} 
+                height={album.images[1].height}
+                aria-label={`Album named ${album.name} by ${artists[0].name}`}
+                /> 
+                <audio controls name="media">
+                    <source src={preview} type="audio/mpeg" />
+                    Your browser does not support the audio element.
+                </audio>
+                <span className="songName">{name}</span>
+                <span className="songAuthor">Author: {artists[0].name}</span>
+                <span className="songAlbum">Album: {album.name}</span>
+                {btn === "add" ? <Add id={id} playlist={playlist} setPlaylist={setPlaylist}/> : null}
+                {btn === "remove" ? <Remove id={id} playlist={playlist} setPlaylist={setPlaylist}/> : null}
             </details>
         </>
     )
