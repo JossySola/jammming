8491b28b1d357d272a5a93d89e858f7a12f4c3f5
Jossy Solá <jossysola@gmail.com>
2023-11-10 01:11:23 -0600

    Adds Effect to work with redirects from Spotify auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,32 @@
 import React, { useEffect, useState } from "react";
 import Search from "./containers/search.jsx";
 import Playlist from "./containers/playlist.jsx";
+// **************************************************
 import accessToken from "./containers/logic/access.js";
-import codeVerifier from "./containers/logic/verifier.js";
-import codeChallenge from "./containers/logic/challenge.js";
-import auth0 from "./containers/logic/Auth0.js";
-import { userAuth } from "./containers/logic/auth.js";
+// **************************************************
 
 export default function App() {
-    useEffect(() => { 
-        console.log(localStorage);
+    const [playlist, setPlaylist] = useState([]);
+    const urlParams = new URLSearchParams(window.location.search);
 
-        const urlParams = new URLSearchParams(window.location.search);
-        const verifierParam = localStorage.getItem('code_verifier');
-        const challengeParam = localStorage.getItem('code_challenge');
-        const state = auth0;
-        const stateParam = localStorage.getItem('state');
-        const stateReceived = urlParams.get('state');
-        const accessParam = localStorage.getItem('access_token');
-        const refreshParam = localStorage.getItem('refresh_token');
-        const codeReceived = urlParams.get('code');
-        const codeParam = localStorage.getItem('code');
+    useEffect(() => {
+        const code = urlParams.get('code');
+        const state = localStorage.getItem('state');
+        const verifier = localStorage.getItem('code_verifier');
+        const access = localStorage.getItem('access_token');
+        const refresh = localStorage.getItem('refresh_token');
 
-        if(
-            (!verifierParam || 
-                verifierParam === "undefined" || 
-                verifierParam === "null") && 
-                (!stateParam || 
-                    stateParam === "undefined" || 
-                    stateParam === "null") && 
-                    (!challengeParam || 
-                        challengeParam === "undefined" || 
-                        challengeParam === "null")) {
-                            localStorage.setItem('code_verifier', codeVerifier);
-                            userAuth(codeChallenge, state);
-        } else if (verifierParam && stateParam && challengeParam) {
-            if(codeReceived) {
-                accessToken(codeReceived, stateReceived, verifierParam);
-            } else if(urlParams.get('error')) {
-                window.localStorage.removeItem('code');
-                window.localStorage.removeItem('refresh_token');
-                window.localStorage.removeItem('access_token');
-                console.error({
-                    type: 'Permissions',
-                    message: 'The permissions were not granted.'
-                });
-            }
+        if(access && refresh) {
+            return;
+        } else if(code) {
+            accessToken(code, state, verifier);
+            console.log('access granted.')
         }
     }, []);
 
-    const [playlist, setPlaylist] = useState([]);
-    
-    const declined = () => {
-        const urlParams = new URLSearchParams(window.location.search);
-        if(urlParams.get('error')) {
-            return <span>Spotify authorization is required</span>
-        }
-    }
-
     return (
         <>
-            { /*redirected()*/ }
-            <span>{declined()}</span>
+            <span>{ urlParams.get('error') ? "Spotify authorization is required" : null }</span>
 
             <main>
                 <Search playlist={playlist} setPlaylist={setPlaylist}/>
